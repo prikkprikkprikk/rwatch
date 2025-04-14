ac444359f4f17acc224817e082d7e7295e558475
Jørn Støylen <jorn@prikkprikkprikk.no>
2025-04-14 10:05:45 +0200

    fix: Redundant assignment to promoted property.

--- src/Command/ConfigAwareCommand.php
+++ src/Command/ConfigAwareCommand.php
@@ -4,9 +4,8 @@ declare(strict_types=1);
 
 namespace RWatch\Command;
 
-use RWatch\Command\CommandInterface;
+use RWatch\Command\Contracts\CommandInterface;
 use RWatch\Config\ConfigInterface;
-use RWatch\IO\IOInterface;
 
 abstract class ConfigAwareCommand implements CommandInterface {
 
@@ -17,6 +16,5 @@ abstract class ConfigAwareCommand implements CommandInterface {
      * @param ConfigInterface $config The app's configuration object.
      */
     public function __construct(protected ConfigInterface $config) {
-        $this->config = $config;
     }
 }
